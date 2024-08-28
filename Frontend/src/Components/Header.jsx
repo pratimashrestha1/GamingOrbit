38e1380b70a424d57689316a34be18c4b3b8b7d9
evillentenxon <romangautam71399@gmail.com>
2024-08-28 21:54:09 +0545

    nav bar update: size decrease

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -2,64 +2,75 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
-import { IoLogOut } from "react-icons/io5";
 import { useGlobalComponent } from '../GlobalComponentContext';
 
 const Header = () => {
-  const { toggleComponentVisibility,componentVisibleFalse } = useGlobalComponent();
-  
+  const { toggleComponentVisibility, componentVisibleFalse } = useGlobalComponent();
+
   return (
     <MainHeader>
-      <ul>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/"><img src="./images/crop thikkako no bg.png" alt="logo" className="logo" /></NavLink></li>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/host">Host</NavLink></li>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/community">Community</NavLink></li>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/news">News</NavLink></li>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/topgames">Top games</NavLink></li>
-        <li><NavLink onClick={componentVisibleFalse} className="navs" to="/about">About us</NavLink></li>
-        <li><FaUserCircle onClick={toggleComponentVisibility} size={30}/></li>
-        <li><IoLogOut size={30}/> </li> 
-      </ul>
+      <div className="nav_menu">
+        <ul>
+          <li><NavLink onClick={componentVisibleFalse} to="/"><img src="./images/crop thikkako no bg.png" alt="logo" className="logo" /></NavLink></li>
+          <li><NavLink onClick={componentVisibleFalse} className="navs" to="/host">Host</NavLink></li>
+          <li><NavLink onClick={componentVisibleFalse} className="navs" to="/community">Community</NavLink></li>
+          <li><NavLink onClick={componentVisibleFalse} className="navs" to="/news">News</NavLink></li>
+          <li><NavLink onClick={componentVisibleFalse} className="navs" to="/topgames">Top games</NavLink></li>
+          <li><NavLink onClick={componentVisibleFalse} className="navs" to="/about">About us</NavLink></li>
+        </ul>
+      </div>
+      <div className="nav_logo">
+        <FaUserCircle className="user_logo" onClick={toggleComponentVisibility} size={30} />
+      </div>
     </MainHeader>
     //Above is the header of logo.
   );
 };
 
 const MainHeader = styled.header`
-background-image: ${({theme})=>theme.colors.gradient};
+background-image: ${({ theme }) => theme.colors.gradient};
 font-family: ${({ theme }) => theme.fontFamily.games};
 background-color: #222;
-img{
-  max-width: 200px;
-  }
-  .navs{
-    text-decoration: none;
-    color: #fff;
-    font-size: 1.5em; 
+display:flex;
+justify-content: space-between;
 
-    &:hover{
-      color: ${({theme})=>theme.colors.logoBlue};
-      border-bottom: 2px solid #FC0D50;
-      transition: 0.3s ease;
-      }
+img{
+  max-width: 150px;
   }
- 
-  ul{
-    width: 100vw;
-    display: flex;
-    justify-content: space-evenly;
-    
-    // gap: 5em;
-    margin: 0px;
-    li{
+
+.nav_menu{
+ul{
+  display:flex;
+  gap:15px;
+  margin: 0;
+
+  li{
       color: #fff;
       list-style-type: none;
       align-content: center;
     }
-  }
+      .navs{
+      text-decoration: none;
+      color: #fff;
+      font-size: 1em; 
 
+      &:hover{
+      color: ${({ theme }) => theme.colors.logoBlue};
+      border-bottom: 2px solid #FC0D50;
+      transition: 0.3s ease;
+      }
+      }
+}
+}
 
-
+.nav_logo{
+  display: flex;
+  align-items: center;
+.user_logo{
+  color: #fff;
+  padding-right: 15px;
+}
+}
 `;
 
 export default Header;
