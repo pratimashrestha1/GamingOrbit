b65d70ecd9fca234cd66a701e014de10756a6558
Evillentenxon <romangautam@Romans-MacBook-Air.local>
2024-10-14 00:31:24 +0545

    top games list

--- Frontend/src/Topgames.jsx
+++ Frontend/src/Topgames.jsx
@@ -24,6 +24,115 @@ function Topgames() {
           </div>
           <h3>Player Unknown BattleGround: PUBG</h3>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Freefire</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <LikeCounter />
+          <h4>Reviews:</h4>
+        </div>
+        <div className='game1'>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/44/PlayerUnknown%27s_Battlegrounds_Mobile.webp/180px-PlayerUnknown%27s_Battlegrounds_Mobile.webp.png" alt="" />
+          <div className="rating">
+            <div className="filling"></div>
+            <img src="./images/White Ratings Father's Day Card.png" alt="" />
+          </div>
+          <h3>Player Unknown BattleGround: PUBG</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nisi quasi deserunt cum repellat, distinctio adipisci perspiciatis ab hic consequatur? Dolores beatae nulla, iure fugit dolore omnis repudiandae doloribus soluta.</p>
           <LikeCounter />
           <h4>Reviews:</h4>
         </div>
@@ -39,8 +148,16 @@ font-family: ${({ theme }) => theme.fontFamily.games};
 
 .top-games-preview{
   background: #fff;
+  padding: 0px 150px;
+
+  h1{
+  text-align: center;
+  padding-top: 50px;
+  }
   
   .game1{
+    box-shadow: 0px 4px 8px #999;
+    padding: 20px 0px 0px 20px;
     .rating{
       max-width: 150px;
       height: 2em;
