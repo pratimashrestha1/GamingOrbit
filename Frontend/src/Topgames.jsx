db5547455fe9c820f819fa4f8147855cf83736be
Evillentenxon <romangautam@Romans-MacBook-Air.local>
2024-10-15 13:52:55 +0545

    top game list complete

--- Frontend/src/Topgames.jsx
+++ Frontend/src/Topgames.jsx
@@ -3,6 +3,7 @@ import Modal from './Components/Modal'
 import Theme from './Components/Intro_theme'
 import styled from 'styled-components'
 import LikeCounter from './Components/Like_counter'
+import { FaTrophy } from "react-icons/fa6";
 
 function Topgames() {
   const [posts, setPosts] = useState([]);
@@ -57,11 +58,81 @@ function Topgames() {
         </div>
       </div>
 
-      {/* list of games */}
+      {/* leaderboard */}
       <div className="game-list">
+        <div className="game-item-1">
+          <FaTrophy className='trophy-icon' />
+          <p>leaderboard</p>
+        </div>
         <div className="game-item">
-          <h2>PUBG</h2>
-          <p>1</p>
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8bfbc4b9-8bd1-4ca2-b19a-165c397e4174/dcnufu3-2b2e4d17-ce67-4ed9-b10f-3eb63771b7a9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhiZmJjNGI5LThiZDEtNGNhMi1iMTlhLTE2NWMzOTdlNDE3NFwvZGNudWZ1My0yYjJlNGQxNy1jZTY3LTRlZDktYjEwZi0zZWI2Mzc3MWI3YTkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.CZ2jTO0PA82QZ7ZIvRvW8zTXlPn_otCbijYq9Hv-oIk" alt="" />
+            <h2>PUBG</h2>
+          </div>
+          <div className="right"><p>1</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f5edeeb1-ecc6-49bb-8d2d-72ab3ab36a87/degn7h4-6c488013-b072-4eea-a386-c614127577e7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y1ZWRlZWIxLWVjYzYtNDliYi04ZDJkLTcyYWIzYWIzNmE4N1wvZGVnbjdoNC02YzQ4ODAxMy1iMDcyLTRlZWEtYTM4Ni1jNjE0MTI3NTc3ZTcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.vxNtMLKMMAk7NYR4i_gRC3uJ4AXpRCjWibuxmJN13vw" alt="" />
+            <h2>Freefire</h2>
+          </div>
+          <div className="right"><p>2</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://freepngimg.com/save/90336-playstation-pc-royale-game-fortnite-battle/512x512" alt="" />
+            <h2>Fortnite</h2>
+          </div>
+          <div className="right"><p>3</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3f5e888-9edb-41d5-9cec-ad06280152e9/d4xtdr9-d8db81d8-02e1-469f-a9b6-23d6ba47d0d7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzZjVlODg4LTllZGItNDFkNS05Y2VjLWFkMDYyODAxNTJlOVwvZDR4dGRyOS1kOGRiODFkOC0wMmUxLTQ2OWYtYTliNi0yM2Q2YmE0N2QwZDcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.J6Eah3wLCgMVTiCnUVzIxR9UvYiuLqjniiGBtfXGwFQ" alt="" />
+            <h2>COD</h2>
+          </div>
+          <div className="right"><p>4</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="./images/clashroyale.png" alt="" />
+            <h2>Clash Royale</h2>
+          </div>
+          <div className="right"><p>5</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3411dfaa-893e-4093-9260-3592bbd25a39/dgaf20k-dcd4f1a9-69a1-4bf1-9d25-be58cee536e4.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM0MTFkZmFhLTg5M2UtNDA5My05MjYwLTM1OTJiYmQyNWEzOVwvZGdhZjIway1kY2Q0ZjFhOS02OWExLTRiZjEtOWQyNS1iZTU4Y2VlNTM2ZTQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SST1beWbLbxU61rmuViwSFlk3BCRdI6c_aLizFCe4Ak" alt="" />
+            <h2>Counter-Strike 2</h2>
+          </div>
+          <div className="right"><p>6</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/557703a2-f0df-4e9e-8006-f1a8a0666122/ddonp58-58ed224c-f931-496d-969a-2f7379849ef8.png/v1/fill/w_512,h_512/cyberpunk_2077_png_icon_by_s7_by_sidyseven_ddonp58-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTEyIiwicGF0aCI6IlwvZlwvNTU3NzAzYTItZjBkZi00ZTllLTgwMDYtZjFhOGEwNjY2MTIyXC9kZG9ucDU4LTU4ZWQyMjRjLWY5MzEtNDk2ZC05NjlhLTJmNzM3OTg0OWVmOC5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.hb8SYjQv4XBeOji7n7D0sA356TQPcUlZ4KUdEiLnqlM" alt="" />
+            <h2>Cyberpunk 2077</h2>
+          </div>
+          <div className="right"><p>7</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/4d04d46d-481e-452a-9bfd-75a8c1cc65a3/dfh0qs5-231e0a2a-1802-4ff1-bf88-6a310863afa1.png" alt="" />
+            <h2>GTA San Andres</h2>
+          </div>
+          <div className="right"><p>8</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9eb92786-5e60-46ac-ae2c-d33af137f691/dad1yjq-7d3b6ae5-2b1c-4570-b65f-0df79372bc85.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzllYjkyNzg2LTVlNjAtNDZhYy1hZTJjLWQzM2FmMTM3ZjY5MVwvZGFkMXlqcS03ZDNiNmFlNS0yYjFjLTQ1NzAtYjY1Zi0wZGY3OTM3MmJjODUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.mMjAEIHvO_Vs9dGLWcD2-ewpqSoXJy-VZVSlGyNSoTU" alt="" />
+            <h2>Pokemon Go</h2>
+          </div>
+          <div className="right"><p>9</p></div>
+        </div>
+        <div className="game-item">
+          <div className="left">
+            <img src="./images/coc.png" alt="" />
+            <h2>COC</h2>
+          </div>
+          <div className="right"><p>10</p></div>
         </div>
       </div>
     </Div>
@@ -112,25 +183,86 @@ font-family: ${({ theme }) => theme.fontFamily.games};
   }
 }
 
+
+// leaderboard**************************************************
+.game-list::before{
+  content:'leaderboard';
+  text-align: center;
+  font-size: 5em;
+  font-weight: bold;
+  position: absolute;
+  rotate: -30deg;
+  top: 300px;
+  left: 300px;
+}
 .game-list{
+position: relative;
   margin-top: 50px;
   display: grid;
-  gap: 10px;
+  background-image: url('./images/Bright Blue Purple Gradient Color and Style Video Background.jpg');
+  // background-size: cover;
+  padding: 50px 0;
 
-  .game-item{
-      margin: auto;
-      width: 80%;
-      background-color: red;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+  .game-item-1{
+    box-shadow: 1px 1px 10px #fff;
+    margin: auto;
+    width: 65%;
+    height: 80px;
+    transform: skew(-15deg, 0);
+    overflow: hidden;
+    border-radius: 0 20px 0 20px;
+    background: rgb(50,64,73);
+    color: #fff;
+    font-size: 0.8em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap:  1em;
+    .trophy-icon{
+      font-size: 6em;
+    }
+  }
+  
+  .game-item{   
+    background: rgba(0, 0, 0, 0.28);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(6.7px);
+    border: 1px solid rgba(255, 255, 255, 0.37);
+    margin: auto;
+    width: 65%;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    transform: skew(-15deg, 0);
+    overflow: hidden;
+    border-radius: 0 20px 0 20px;
+    
+    .left{
+      img{
+        max-height: 100%;
+        margin-right: 1em;
+      }
+      img,h2{
+        display: inline-block;
+        vertical-align: middle;
+      }
+    }
+    .right{
+      width: 20%;   
+      }
+  }
 
-      h2{
-        font-size: 100px;
+    h2{
+      margin: 0;
+      font-size: 2em;
+      color: #fff;
       }
       p{
-        font-family: ${({theme})=>theme.fontFamily.rank}};
-        font-size: 100px;
-    }
+        margin: 0;
+        text-align: center;
+        font-size: 5em;
+        font-family: ${({ theme }) => theme.fontFamily.rank};   
+        color: #fff;     
+      }
 }
 `
