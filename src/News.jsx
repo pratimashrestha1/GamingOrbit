920d3d5915681d5a7cea30c73866f98969717a5a
evillentenxon <romangautam71399@gmail.com>
2024-11-03 09:39:12 +0545

    checking git remote connection

--- src/News.jsx
+++ src/News.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import Modal from './Components/Modal'
 import Theme from './Components/Intro_theme'
+import styled from 'styled-components'
 
 function News() {
   return (
-    <>
-    <Modal/>
-    <Theme title="News" 
-    image='./images/news.png'
-    width='300px'
-    description=' Stay up-to-date with the latest gaming industry news, including updates on game releases, patches, esports events, and community-driven content.'
-    />
-    </>
+    <Div>
+      <Modal />
+      <Theme title="News"
+        image='./images/news.png'
+        width='300px'
+        description=' Stay up-to-date with the latest gaming industry news, including updates on game releases, patches, esports events, and community-driven content.'
+      />
+
+      <div className='check'><h1>checking</h1></div>
+    </Div>
   )
 }
 
 export default News
+
+const Div= styled.div`
+.check{
+  background: green;
+  color: red;
+  height: 400px;
+  width: 100vw;
+}
+`
